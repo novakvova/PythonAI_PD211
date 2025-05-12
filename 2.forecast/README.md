b3f1a7cdbcbb1859ae6968e7ceab1a438502cda3
novakvova <novakvova@gmail>
2025-05-12 17:44:51 +0300

    Begin learn usd

---
+++ 2.forecast/README.md
@@ -0,0 +1,12 @@
+## Прогнозування курсу валют НБУ
+
+```
+py -m venv .venv
+.venv\Scripts\activate.bat
+python.exe -m pip install --upgrade pip
+pip install pandas matplotlib scikit-learn
+
+python main.py
+deactivate
+
+```
---
+++ 2.forecast/main.py
@@ -0,0 +1,19 @@
+#для оброки табличних даних
+import pandas as pd
+#для числових масивів
+import numpy as np
+#для побудови графіків
+import matplotlib.pyplot as plt
+
+#для машиного навчання
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+#для обчислення майбутніх дат.
+from datetime import timedelta
+
+
+
+
+
